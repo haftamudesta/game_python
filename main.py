952ff12b6ceec7cb481aca63a540a58de1b93ddd
haftamudesta <desta451616@hotmail.com>
2025-07-30 06:58:24 -0800

    Add rectangle surface

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ surf = pygame.Surface((100, 200))
 surf.fill("blue")
 x = 100
 player_surface = pygame.image.load("images/player.png")
+
+player_rect = pygame.Rect(0, 50, 0, 0)
 star_surface = pygame.image.load("images/star.png")
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT))
                   for i in range(20)]
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     display_surface.fill('purple')
-    x += 0.1
-    display_surface.blit(player_surface, (x, 150))
+    display_surface.blit(player_surface, (player_rect))
     for position in star_positions:
         display_surface.blit(star_surface, position)
     pygame.display.update()
